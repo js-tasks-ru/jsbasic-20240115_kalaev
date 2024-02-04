5762cc31b66f9a5d2c4bb3cbd8e74246ef013f61
Hamid Kalaev <gamidkalaev@yandex.ru>
2024-02-04 13:51:32 +0300

    Задачи одуль 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,18 @@
+
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (let i in salaries) {
+    isFinite(salaries[i]) ? result += salaries[i] : result;
+  }
+  return result;
 }
+sumSalary(salaries)
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,19 @@
 let calculator = {
-  // ваш код
+  read(a,b) { //при вызове присваиваются значения
+    this.a = a;
+    this.b = b;
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
 };
 
-// НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
-window.calculator = calculator; // делает ваш калькулятор доступным глобально
+calculator.read(3,5);
+console.log(calculator.sum()); // 8
+console.log(calculator.mul()); // 15
